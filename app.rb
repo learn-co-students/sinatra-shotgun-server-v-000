3c36ee4f125865c845823a9d198c5b88304ad794
Kathleen DaSilva <kayjsmyth@gmail.com>
2016-09-07 18:26:58 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,9 +1,9 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-
-  get '/' do 
-    "Welcome to your app!!!! "
+#shotgun gets Rack to reload when we refresh, so when we change and save this file, we can see the results without starting a new server
+  get '/' do
+    "Started my server using shotgun!"
   end
 
 end
