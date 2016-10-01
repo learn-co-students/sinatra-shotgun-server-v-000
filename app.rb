26fbd0d2ea4027cf38c6c5ddeb45a52825634858
Sammy Wu <wu.sammy@gmail.com>
2016-10-01 21:57:58 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,8 +2,12 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do 
-    "Welcome to your app!!!! "
+  get '/' do
+    "Started my server using shotgun! bang bang!"
   end
 
 end
+
+
+#shotgun gem - This gem lets us start Rack
+#to have a development server running to test our app.
