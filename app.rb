b22598cc52c85b98d7d8024dd059c85515daee6d
Scott Bewick <bewick.scott@gmail.com>
2016-11-30 12:47:10 -0500

    Done.

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! "
+    "Welcome to your app!!!! Hey you guys!!!! Let me guess, his will show in the browser"
   end
 
 end
