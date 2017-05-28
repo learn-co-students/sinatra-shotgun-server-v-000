86333fb592b0da8883fd18827468abaffb62bda9
Menachem Hornbacher <me@mendel.tech>
2017-05-28 17:37:17 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do 
-    "Welcome to your app!!!! "
+  get '/' do
+    "Edited in real time whith Shotgun"
   end
 
 end
