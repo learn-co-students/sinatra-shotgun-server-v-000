9aebb5d71e2ee42a38b0e511f4b6cd00cc3f35ad
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-02-05 23:29:55 -0500

    Witnessed the power of Shotgun

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! "
+    "Welcome to your app!!!!  I BUILT THIS!! And I'm running it with Shotgun! There is something psychotic and deeply unhealthy about this obsessions with maximizing efficiency."
   end
 
 end
