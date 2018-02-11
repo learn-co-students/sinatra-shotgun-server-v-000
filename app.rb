c965584c31312a7b3a1473c13c75b6d4f800a177
Thomas Viles <thomas.c.viles.jr@gmail.com>
2018-02-11 20:21:37 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do 
-    "Welcome to your app!!!! "
+  get '/' do
+    "Welcome to your app!!!! Khal Drogo is the best!!! The GoT character AND My Doggo!!!</br>I started this server using SHOTGUN!"
   end
 
 end
