92c70e52b59975c57c34c9c0daa839b1da044996
jljardon <github email address>
2018-02-25 18:56:06 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,9 +1,7 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-
-  get '/' do 
-    "Welcome to your app!!!! "
-  end
-
+   get '/' do
+     "Started my server using Shotgun!"
+   end
 end
