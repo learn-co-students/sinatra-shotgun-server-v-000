9bdc84a25aa934e321a9636e3b4927487d4ec81d
EC2 Default User <ec2-user@ip-172-31-14-103.us-east-2.compute.internal>
2018-02-26 18:40:08 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! "
+    "Welcome to your app!!!! I MADE A CHANGE!! it's very cool"
   end
 
 end
