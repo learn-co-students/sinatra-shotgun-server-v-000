794040d8f8c6f645413a8ccee80affecba59e149
TylerP33 <tyler3321@gmail.com>
2018-02-28 12:03:23 -0600

    Done.

--- app.rb
+++ app.rb
@@ -3,7 +3,12 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! "
+    "Started my server using Shotgun! "
   end
 
 end
+
+#how this differs from rack is that it actually changed the
+#the output when we changed the code, thanks to the code
+#reload
+#WE SHOTGUNNED IT BABY!
