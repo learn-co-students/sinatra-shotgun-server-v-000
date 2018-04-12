af73b6c72313d8d5a6afb55eb941999ec2ca0c50
Michael St. Marie <michael.landrum.st.marie@gmail.com>
2018-04-12 13:59:45 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,8 +2,9 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do 
-    "Welcome to your app!!!! "
+  get '/' do
+    "Welcome to your app!!!! " "------Sinatra-----"
+    "Today is Thursday, April 12, 2018, hello world"
   end
 
 end
