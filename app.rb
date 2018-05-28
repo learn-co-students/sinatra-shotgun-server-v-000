7d145fdd0b9172d9e2c37d2c32a582839e1dc658
eyi1 <ej7729@gmail.com>
2018-05-28 15:07:59 -0400

    Done.

--- app.rb
+++ app.rb
@@ -3,8 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Started my server using Shotgun!
-    adding another line of code"
+    "Started my server using Shotgun!"
   end
 
 end
