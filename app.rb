3e57d014dd4fecc1ebe2bbeec0cd18388373bdb8
emanuelbierman <emanuel.bierman@gmail.com>
2018-09-02 21:17:13 -0400

    Second commit

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! "
+    "Welcome to your app!!!! This is so cool. Wow, it really updates!"
   end
 
 end
