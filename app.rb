ff51ecc3737bfdeee536c9947a2c809223f503c6
Kari O'Neal <triadfirewood@gmail.com>
2019-07-12 13:25:04 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! "
+    "Shotgun it is!!  Looking forward to a great job where I can work from home within 1 year!!"
   end
 
 end
