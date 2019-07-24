1db89800eb702f9454bb354a5781e02a0268ba0e
Marcus Edwards 111 <Soulo@Marcuss-MacBook-Pro.local>
2019-07-24 15:45:41 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,9 +1,10 @@
+
 require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do 
-    "Welcome to your app!!!! "
+  get '/' do
+    "started my server using Shotgun!"
   end
 
 end
