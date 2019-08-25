624f268e6f47e226be23bf4fa2477190acbfa18e
David Forer <david.j.forer@gmail.com>
2019-08-25 05:41:23 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,7 +3,11 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! "
+    "Welcome to your app!!!! I BUILT THIS! SO Exciting!"
+    
+    "Started my server using Shotgun!"
+    
+    
   end
 
 end
